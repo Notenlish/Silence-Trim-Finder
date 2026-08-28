07effc1a616b2022d45fcdca339bac91df87d9f9
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-08-28 12:28:17 +0300

    nvm

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    base: "/silence-trim-finder",
     plugins: [tailwindcss()],
   },
 });
