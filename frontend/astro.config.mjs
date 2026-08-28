ef9d839570042542f57aac337516dad2ba391558
Notenlish <71970100+Notenlish@users.noreply.github.com>
2026-08-28 12:24:47 +0300

    idk at this point

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/silence-trim-finder',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    base: "/silence-trim-finder",
+    plugins: [tailwindcss()],
+  },
 });
